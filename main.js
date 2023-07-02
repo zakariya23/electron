05d4c864294f9c44618ae53a4c255e4a35246fdf
zakariya <commanderzee455@gmail.com>
2023-07-02 18:29:49 -0400

    login testing

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Modules
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, webContents} = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -10,6 +10,7 @@ function createWindow () {
 
   mainWindow = new BrowserWindow({
     width: 1000, height: 800,
+    x: 100, y: 100,
     webPreferences: {
       // --- !! IMPORTANT !! ---
       // Disable 'contextIsolation' to allow 'nodeIntegration'
@@ -20,11 +21,37 @@ function createWindow () {
   })
 
   // Load index.html into the new BrowserWindow
-  mainWindow.loadFile('index.html')
+  // mainWindow.loadFile('index.html')
+  mainWindow.loadURL('http://httpbin.org/basic-auth/user/passwd')
 
   // Open DevTools - Remove for PRODUCTION!
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
+  let wc = mainWindow.webContents
+
+//  wc.on('did-finish-load', ()=>{
+//   console.log('finished loading')
+//  })
+
+//  wc.on('dom-ready', ()=>{
+//   console.log('DOM ready')
+//  })
+
+//  wc.on('new-window', (e, url)=>{
+//   e.preventDefault()
+//   console.log(`creating a new window for url: ${url}`)
+//  })
+
+wc.on('login', (e, request, authInfo, callback) =>{
+  console.log('logging in')
+  callback('user','passwd')
+})
+
+wc.on('did-navigate', (e, url, statusCode, message)=>{
+
+    console.log(`navigated to ${url}`)
+    console.log(statusCode, message)
+   })
   // Listen for window being closed
   mainWindow.on('closed',  () => {
     mainWindow = null
